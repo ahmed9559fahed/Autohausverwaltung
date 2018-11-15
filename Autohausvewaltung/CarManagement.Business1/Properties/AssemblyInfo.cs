a7ba89b334185ce92a89aa5ef6a09c7d8b62035d
ahmed fahed <fahed@gmail.com>
2018-11-15 19:19:57 +0100

    Implement GUI

--- Autohausvewaltung/CarManagement.Business1/Properties/AssemblyInfo.cs
+++ Autohausvewaltung/CarManagement.Business1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("CarManagement.Business1")]
+[assembly: AssemblyTitle("CarManagement.Business")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarManagement.Business1")]
+[assembly: AssemblyProduct("CarManagement.Business")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
