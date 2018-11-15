8e70fcd441163b55dc797999c7165addc837624c
ahmed1234 <alifahed9559@gmail.com>
2018-11-15 10:53:30 +0100

    Convert to new sdk format

--- Autohausvewaltung/CarManagement.Data1/DBModels/DbContext.cs
+++ Autohausvewaltung/CarManagement.Data1/DBModels/DbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using CarManagement.Data1.Migrations;
 using CarManagement.Data1.ModelMapping;
 
