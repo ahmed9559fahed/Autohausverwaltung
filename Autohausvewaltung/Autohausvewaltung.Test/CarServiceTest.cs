6e5f5b011bb1014561e1fe642cd9bcc0fc15039d
ahmed fahed <fahed@gmail.com>
2018-11-15 20:03:25 +0100

    add test

---
+++ Autohausvewaltung/Autohausvewaltung.Test/CarServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CarManagement.Test
+{
+    [TestFixture]
+    public class CarServiceTest
+    {
+        public CarServiceTest()
+        {
+            
+        }
+
+        [Test]
+        public void tet1()
+        {
+            Assert.AreEqual(true,true);
+        }
+    }
+}
