6fc9aa2775d547f609a8f164e03a3d201cfccdb7
ahmed1234 <alifahed9559@gmail.com>
2018-11-18 15:03:34 +0100

    Add Unit Test

--- Autohausvewaltung/Autohausvewaltung.Test/CarServiceTest.cs
+++ Autohausvewaltung/Autohausvewaltung.Test/CarServiceTest.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarManagement.Business.Interface;
+using CarManagement.Business.Models;
+using CarManagement.Business.Service;
 using NUnit.Framework;
 
 namespace CarManagement.Test
@@ -10,15 +13,43 @@ namespace CarManagement.Test
     [TestFixture]
     public class CarServiceTest
     {
+        public CarService CarService { get; set; } 
         public CarServiceTest()
         {
             
         }
 
         [Test]
-        public void tet1()
+        public void EntriedCarWithoutType_WhenAddNewCar_ThenCarNotAdded()
+        { CarService=new CarService();
+        var car =new Car();
+            car.Id=Guid.NewGuid();
+            car.Address = "Test";
+            car.FirstName = "Afa";
+            car.Color = "WHite";
+            car.Name = "Fahed";
+            var result= CarService.AddCar(car);
+
+            Assert.IsFalse(result);
+        }
+
+
+        [Test]
+        public void EntriedCarWithAllFields_WhenAddNewCar_ThenCarWasAdded()
         {
-            Assert.AreEqual(true,true);
+            CarService = new CarService();
+            var car = new Car();
+            car.Id = Guid.NewGuid();
+            car.Address = "Test";
+            car.FirstName = "Afa";
+            car.Color = "WHite";
+            car.Name = "Fahed";
+            car.Type = "BMW";
+            car.CostumerId=Guid.NewGuid();
+           
+            var result = CarService.AddCar(car);
+
+            Assert.IsTrue(result);
         }
     }
 }
